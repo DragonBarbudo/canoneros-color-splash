acda4d317fdc9f5b99fc68f69889f18df10efbf3
Albin Rodriguez <ealbinu@gmail.com>
2025-05-29 18:21:59 -0600

    logo fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
       <div className="container mx-auto px-4 py-6">
         <div className="flex items-center justify-center">
           <img
-            src="/lovable-uploads/369f5830-0a6c-4fda-a894-538dfb68ffa1.png"
+            src="/paint/369f5830-0a6c-4fda-a894-538dfb68ffa1.png"
             alt="Fan Cañoneros Kids"
             className="h-16 md:h-20 object-contain"
           />
